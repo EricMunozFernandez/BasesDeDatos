48bac6a6b3aff9c25dad48f6660d8f3e31d7eeeb
Eric <eric.munoz@ikasle.egibide.org>
2018-12-05 13:45:56 +0100

    SQL TAREA 0 BASICA Y TAREA 1

---
+++ Tareas_a_realizar_1_select_basicas_EquipoBase_Final.sql
@@ -0,0 +1,55 @@
+/*       
+    1. A partir de la tabla EMPLE, obtener el APELLIDO, SALARIO y DEPT_NO
+    de los empleados cuyo salario sea mayor de 200000 de los departamentos 10 y 20.*/
+    
+    SELECT APELLIDO, SALARIO, DEPT_NO
+    FROM EMPLE
+    WHERE SALARIO > 2000
+    AND DEPT_NO IN(10,20); 
+    
+    /*2.  A partir de la tabla EMPLE, obtener aquellos empleados cuyo apellido empiece por una ‘J’. */
+    
+    SELECT APELLIDO
+    FROM EMPLE
+    WHERE APELLIDO LIKE 'J%';
+    
+    /*3. Obtener aquellos empleados cuyo apellido tengan una ‘R’ en la segunda posición. */
+    
+    SELECT APELLIDO
+    FROM EMPLE
+    WHERE APELLIDO LIKE '_R%'; 
+    
+    /*4. Obtener aquellos empleados cuyo apellido empiece por ‘A’ y tengan una ‘O’ en su interior.*/
+    
+    
+       
+    /*5. A partir de la tabla EMPLE, obtener el apellido y el salario
+    de los empleados cuyo salario esté comprendido entre 150000 y 200000. */
+       
+    /*6. A partir de la tabla EMPLE, obtener el apellido y el salario
+    de los empleados cuyo salario NO esté comprendido entre 150000 y 200000. */
+       
+    /*7. Consultar los apellidos de la tabla EMPLE cuyo oficio sea ‘VENDEDOR’, ‘ANALISTA’ o ’EMPLEADO’.*/
+       
+    /*8. Consultar los apellidos de la tabla EMPLE cuyo 
+    oficio no sea ni ‘VENDEDOR’, ni ‘ANALISTA’ ni ’EMPLEADO’.*/
+       
+    /*9.  A partir de la tabla EMPLE, obtener el apellido de los empleados cuyo departamento sea 10 o 30.*/
+    
+    /*10. Consultar los empleados cuyo departamento no sea ni 10 ni 30. */
+       
+    /*11. Consultar los apellidos de los empleados cuya comisión es nula. */
+       
+    /*12. Consultar los apellidos de los empleados cuya comisión no es nula.*/
+       
+    /*13. Mostrar el apellido y el oficio de los empleados que no tienen director.*/ 
+       
+    /*14. Consultar los oficios de la empresa pero sin que aparezcan repeticiones.*/
+       
+    /*15. Seleccionar de la tabla EMPLE a todos los empleados 
+    del departamento 20. Además, la consulta debe aparecer ordenada 
+    por APELLIDO. En el caso de que dos empleados tengan el mismo apellido, 
+    ordenarlos de mayor a menor salario. Los campos que hay que consultar son: 
+    número de empleado, apellido, oficio y número de departamento. */
+       
+    /*16. Consulta de los empleados cuyo oficio sea ‘ANALISTA’ ordenado por número de empleado. */    
