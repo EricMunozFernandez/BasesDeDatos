6c0bf39d1ef29b5f29229fd8c78c4cfbf6564e31
Eric <eric.munoz@ikasle.egibide.org>
2018-12-20 10:57:15 +0100

    TAREA 5 HASTA EL 4

--- Tareas_a_realizar_5_1_varias_tablas.sql
+++ Tareas_a_realizar_5_1_varias_tablas.sql
@@ -47,32 +47,51 @@ AND UPPER(LOC) IN ('MADRID','BARCELONA');
 JEFE de un empleado indica el numero de empleado de su jefe.
 */
 
-SELECT EMPLE.EMP_NO,
-EMPLE.APELLIDO,
-EMPLE.ID_JEFE
-FROM EMPLE, DEPART
-WHERE EMPLE.DEPT_NO=DEPART.DEPT_NO 
-AND UPPER(LOC) IN ('MADRID','BARCELONA');
+SELECT E.EMP_NO,
+E.APELLIDO AS EMPLEADO,
+J.APELLIDO AS JEFE
+FROM EMPLE E, EMPLE J
+WHERE E.ID_JEFE=J.EMP_NO;
+--EL NUMERO DE JEFE DEL EMPLEADO ES EL NUMERO EMPLEADO DEL JEFE EN SI.
+
+
 
 /*
 4. Visualizar el apellido, el oficio y la localidad de los departamentos donde trabajan los ANALISTAS.
 */
 
-
+SELECT APELLIDO,
+OFICIO,
+DEPART.LOC
+FROM EMPLE,DEPART
+WHERE EMPLE.DEPT_NO=DEPART.DEPT_NO 
+AND UPPER(OFICIO)='ANALISTA';
 
 /*
 5. Mostrar para cada empleado quién es su jefe (apellido ). Si no tiene jefe,
 mostrar ausencia de valor(NULL).
 */
 
-
-
+SELECT E.EMP_NO,
+E.APELLIDO AS EMPLEADO,
+J.APELLIDO AS JEFE
+FROM EMPLE E, EMPLE J
+WHERE E.ID_JEFE=J.EMP_NO(+);
+--EL (+) ES PARA MOSTRAR LOS NULL DE JEFE.
+/*
+EN VEZ DE J.APELLIDO AS JEFE 
+NVL(J.APELLIDO,'NO TIENE JEFE') AS JEFE
+SI QUEREMOS QUE SALGA COMO SIN JEFE EL NULL
+*/
 /*
 6. Mostrar para todos los departamentos que tenemos en la empresa los apellidos de los 
 empleados que pertenecen a cada uno de ellos. Si algún departamento está vacío, 
 mostrar ausencia de valor NULL.
 */
 
-
+SELECT APELLIDO,
+DEPART.DEPT_NO
+FROM EMPLE, DEPART
+WHERE EMPLE.DEPT_NO(+)=DEPART.DEPT_NO;
 
 /*
